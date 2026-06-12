035e6190a814d8acee7f42901842b4f63a017e14
Ubuntu <ubuntu@ip-172-31-23-162.ec2.internal>
2026-06-12 08:41:45 +0000

    built 11

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "CI/CD Deployement successful built-10"
+    return "CI/CD Deployement successful built-11"
 
 
 if __name__ == "__main__":
